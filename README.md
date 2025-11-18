2a09bcba4a9acf60a4f61daf8ab53b1692a31dd3
scapone1997 <simo.capone.1997.7@outlook.it>
2025-11-18 20:07:44 +0100

    base

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # big-data-esercizi
+
+# Inerfaccia redis
+https://github.com/qishibo/AnotherRedisDesktopManager/releases
---
+++ consumer.py
@@ -0,0 +1,12 @@
+import redis
+
+r = redis.Redis(host="localhost", port=6379)
+
+pubsub = r.pubsub()
+pubsub.subscribe("demo")
+
+print("In attesa di messaggi...")
+
+for msg in pubsub.listen():
+    if msg["type"] == "message":
+        print("Ricevuto:", msg["data"].decode())

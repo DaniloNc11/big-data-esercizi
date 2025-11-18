84bb4c3d0e4a41a09038175217ff979ea965db56
scapone1997 <simo.capone.1997.7@outlook.it>
2025-11-18 20:28:09 +0100

    base

--- consumer.py
+++ consumer.py
@@ -1,12 +1,33 @@
 import redis
+import boto3
+from botocore.exceptions import ClientError
 
 r = redis.Redis(host="localhost", port=6379)
-
 pubsub = r.pubsub()
 pubsub.subscribe("demo")
+
+s3 = boto3.client(
+    "s3",
+    endpoint_url="http://localhost:9000",
+    aws_access_key_id="admin",
+    aws_secret_access_key="admin123"
+)
+
+bucket = "my-bucket"
+filename = "messaggio.txt"
+
+try:
+    s3.head_bucket(Bucket=bucket)
+except ClientError:
+    s3.create_bucket(Bucket=bucket)
 
 print("In attesa di messaggi...")
 
 for msg in pubsub.listen():
     if msg["type"] == "message":
-        print("Ricevuto:", msg["data"].decode())
+        text = msg["data"].decode()
+        print("Ricevuto:", text)
+
+        #TODO: trasformare il messaggio in upper case
+
+        #TODO: inserire l'oggetto dentro il bucket

a3734a8cc7d2fbbde6893ff6c44aceed808b3881
DaniloNc11 <daeffe.04@gmail.com>
2025-11-19 15:04:32 +0100

    commit

--- consumer.py
+++ consumer.py
@@ -1,5 +1,6 @@
 import redis
 import boto3
+import base64
 from botocore.exceptions import ClientError
 from datetime import datetime
 
@@ -9,14 +10,20 @@ pubsub.subscribe("demo")
 
 s3 = boto3.client(
     "s3",
-    endpoint_url="http://localhost:9000",
+    endpoint_url="http://192.168.7.38:9000",
     aws_access_key_id="admin",
     aws_secret_access_key="admin123"
 )
 
+uuid = __import__('uuid')
 bucket = "my-bucket"
+<<<<<<< Updated upstream
 filename = "messaggio.txt"
 count = 0
+=======
+filename = "cyberchallenge.zip"
+uuidkey = str(uuid.uuid4()) + ".zip"
+>>>>>>> Stashed changes
 
 try:
     s3.head_bucket(Bucket=bucket)
@@ -27,6 +34,7 @@ print("In attesa di messaggi...")
 
 for msg in pubsub.listen():
     if msg["type"] == "message":
+<<<<<<< Updated upstream
         text = msg["data"].decode()
         print("Ricevuto:", text)
 
@@ -46,3 +54,12 @@ for msg in pubsub.listen():
         # "3 | 2025-01-01T12:30:00 | MESSAGGIO 1"
 
         # TODO 5: salvare il messaggio finale su MinIO.
+=======
+        encoded = msg["data"].decode()
+        try:
+            file_bytes = base64.b64decode(encoded)
+            s3.put_object(Bucket=bucket, Key=uuidkey, Body=file_bytes)
+            print(f"Caricato '{filename}' nel bucket '{bucket}' come oggetto '{uuidkey}'")
+        except Exception as e:
+            print("Errore nella decodifica o nel caricamento:", e)
+>>>>>>> Stashed changes
--- producer.py
+++ producer.py
@@ -1,8 +1,15 @@
 import redis
+import base64
 
+# Connessione a Redis
 r = redis.Redis(host="localhost", port=6379)
 
-message = "Messaggio 1"
-r.publish("demo", message)
+# Leggi e codifica il file
+with open("C:\\Users\\A829apulia\\Desktop\\Cyberchallenge.zip", "rb") as f:
+    file_bytes = f.read()
+    encoded = base64.b64encode(file_bytes).decode()
 
-print("Inviato:", message)
+# Pubblica il messaggio codificato
+r.publish("demo", encoded)
+
+print("Inviato: cyberchallenge.zip codificato in base64")
